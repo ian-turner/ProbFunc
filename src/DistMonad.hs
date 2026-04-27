622269097d384a14ac22b767b2719d627cef36e5
ian-turner <i.r.turner55@gmail.com>
2026-04-27 00:51:11 -0400

    monad definition + blackjack example

--- src/DistMonad.hs
+++ src/DistMonad.hs
@@ -4,19 +4,17 @@ module DistMonad where
 
 type Probability = Float
 data Dist a = D {unD :: [(a, Probability)]}
-
-
-instance Functor Dist where
-  fmap :: (a -> b) -> Dist a -> Dist b
-  fmap f (D d) = D [(f x, p) | (x,p) <- d]
-
-
-instance Applicative Dist where
-  pure x = D [(x,1)]
-  (D fs) <*> (D x) = undefined
-
+              deriving (Show, Eq)
 
 instance Monad Dist where
   return x    = D [(x,1)]
   (D d) >>= f = D [(y,q*p) | (x,p) <- d,
                              (y,q) <- unD (f x)]
+
+instance Functor Dist where
+  fmap :: (a -> b) -> Dist a -> Dist b
+  fmap f (D d) = D [(f x, p) | (x,p) <- d]
+
+instance Applicative Dist where
+  pure x = D [(x,1)]
+  (D fd) <*> (D xd) = D [(f x, p*q) | (x,p) <- xd, (f,q) <- fd]
--- test/Main.hs
+++ test/Main.hs
@@ -5,9 +5,5 @@ import Test.QuickCheck
 import ProbFunc
 
 
-prop :: Int -> Bool
-prop x = ((\y -> y) x) == x
-
-
 main :: IO ()
 main = quickCheck prop
