65cce8131022e8376184317dd9b7fdf68d19d666
ian-turner <i.r.turner55@gmail.com>
2026-04-27 01:25:08 -0400

    basic setup for blackjack

--- examples/blackjack/BlackJack.hs
+++ examples/blackjack/BlackJack.hs
@@ -1,4 +1,17 @@
 module BlackJack where
 
+import DistMonad
 
-data Action = Hit | Stay deriving (Show, Eq)
+
+data Card = Ace | Two | Three | Four | Five       -- | Defining cards
+          | Six | Seven | Eight | Nine | Ten
+          | Jack | Queen | King
+          deriving (Show, Eq)                     
+data Hand = H Int Bool deriving (Show, Eq)        -- | Hand of player: count, hard/soft flag (ace or not)
+data Action = Hit | Stay deriving (Show, Eq)      -- | Hit (draw another card) or stay (no more cards)
+data Outcome = Win | Lose deriving (Show, Eq)     -- | Outcome of hand - either win or lose
+type Strategy = (Hand -> Card -> Action)          -- | Choice of action based on player hand and dealer card
+
+-- | Functions for game actions - drawing cards, hit/stay, etc.
+drawCard :: Dist Card
+drawCard = uniform [Ace,Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Jack,Queen,King]
--- src/DistMonad.hs
+++ src/DistMonad.hs
@@ -2,6 +2,7 @@
 module DistMonad where
 
 
+-- | Probability monad definition
 type Probability = Float
 data Dist a = D {unD :: [(a, Probability)]}
               deriving (Show, Eq)
@@ -18,3 +19,8 @@ instance Functor Dist where
 instance Applicative Dist where
   pure x = D [(x,1)]
   (D fd) <*> (D xd) = D [(f x, p*q) | (x,p) <- xd, (f,q) <- fd]
+
+
+-- | Utility functions
+uniform :: [a] -> Dist a
+uniform xs = let n = fromIntegral (length xs) in D [(x,1/n) | x <- xs]
